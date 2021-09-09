953d2b18a4337fc7b24e5693d49bec408bc8a6fc
Roman Engelke <engelke.roman@gmail.com>
2021-09-09 21:56:24 +0200

    Switch upgrade

--- src/main/java/LadenAlarm.java
+++ src/main/java/LadenAlarm.java
@@ -10,8 +10,10 @@ public class LadenAlarm{
         //System.out.println("Output 30 customers:");
         //System.out.println(howMuchCustomers(30));
 
-        int customers = -22;
+        int customers = 31;
         System.out.println(howMuchCustomers(customers));
+
+        System.out.println(howMuchCustomersSwitch("green", customers));
 
     }
 
@@ -19,5 +21,31 @@ public class LadenAlarm{
         if (customers < 30) return "Maximale Personenzahl nicht ueberschritten";
         else if (customers > 30) return "Zu viele Personen";
         else return "edgeCase";
+    }
+
+    public static String howMuchCustomersSwitch(String code, int customers){
+
+        String resultSwitch;
+
+        switch(code){
+
+            case "red":
+                resultSwitch = "Zu viele Personen";
+                break;
+
+            case "yellow":
+                if (customers<=30) resultSwitch = "Maximale Personenzahl nicht ueberschritten";
+                else resultSwitch = "Zu viele Personen";
+                break;
+
+            case "green":
+                if (customers<=60) resultSwitch = "Maximale Personenzahl nicht ueberschritten";
+                else resultSwitch = "Zu viele Personen";
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + code);
+        }
+        return resultSwitch;
+
     }
 }
--- src/test/java/LadenAlarmTest.java
+++ src/test/java/LadenAlarmTest.java
@@ -47,4 +47,80 @@ public class LadenAlarmTest {
         Assertions.assertEquals("edgeCase", result3);
     }
 
+    @Test
+    @DisplayName("validCustomersGreenSwitch")
+    public void validCustomerSizeGreenSwitch(){
+
+        // Given
+        int case4 = 29;
+
+        // When
+        String result4 = LadenAlarm.howMuchCustomersSwitch("green",case4);
+
+        // Then
+        Assertions.assertEquals("Maximale Personenzahl nicht ueberschritten", result4);
+    }
+    @Test
+    @DisplayName("invalidCustomersGreenSwitch")
+    public void invalidCustomerSizeGreenSwitch(){
+
+        // Given
+        int case5 = 61;
+
+        // When
+        String result5 = LadenAlarm.howMuchCustomersSwitch("green", case5);
+
+        // Then
+        Assertions.assertEquals("Zu viele Personen", result5);
+
+    }
+
+    @Test
+    @DisplayName("validCustomersYellowSwitch")
+    public void validCustomerSizeYellowSwitch(){
+
+        // Given
+        int case6 = 29;
+
+        // When
+        String result6 = LadenAlarm.howMuchCustomersSwitch("yellow", case6);
+
+        // Then
+        Assertions.assertEquals("Maximale Personenzahl nicht ueberschritten", result6);
+
+    }
+
+    @Test
+    @DisplayName("invalidCustomersYellowSwitch")
+    public void invalidCustomerSizeYellowSwitch(){
+
+        // Given
+        int case7 = 31;
+
+        // When
+        String result7 = LadenAlarm.howMuchCustomersSwitch("yellow", case7);
+
+        // Then
+        Assertions.assertEquals("Zu viele Personen", result7);
+
+    }
+
+    @Test
+    @DisplayName("invalidCustomersRedSwitch")
+    public void invalidCustomerSizeRedSwitch(){
+
+        // Given
+        int case8 = 29;
+
+        // When
+        String result8 = LadenAlarm.howMuchCustomersSwitch("red", case8);
+
+        // Then
+        Assertions.assertEquals("Zu viele Personen", result8);
+
+    }
+
+
+
+
 }
